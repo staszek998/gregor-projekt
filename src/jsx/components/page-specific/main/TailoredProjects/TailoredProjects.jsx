eec2a0e17e2031d9c8cbd085b7f843db0392eac4
Stanislaw <gregor.stanislaw@hotmail.com>
2018-11-18 12:20:31 +0100

    Add copy to the TailoredProjects section

--- src/jsx/components/page-specific/main/TailoredProjects/TailoredProjects.jsx
+++ src/jsx/components/page-specific/main/TailoredProjects/TailoredProjects.jsx
@@ -1,14 +1,57 @@
 import React from "react";
 
+import "./_TailoredProjects.scss";
+
 const TailoredProjects = () => (
   <div className="row tailored-projects bg-white py-5">
     <div className="col-12">
-      <h1 className="display-4 text-center">Projekty szyte na miarę</h1>
+      <h1 className="display-4 text-center">Kompleksowa inżynieria</h1>
 
-      <div className="row justify-content-center mt-5">
-        <div className="col-3">col</div>
-        <div className="col-3">col</div>
-        <div className="col-3">col</div>
+      <div className="row justify-content-center align-items-center mt-5 pt-4">
+        <div className="col-4 text-center">
+          <i className="fas fa-archway" />
+        </div>
+        <div className="col-4 text-center">
+          <i className="fas fa-pencil-ruler" />
+        </div>
+        <div className="col-4 text-center">
+          <i className="fas fa-binoculars" />
+        </div>
+      </div>
+      <div className="row text-center mt-4">
+        <div className="col-4">
+          <h2>Architektura</h2>
+        </div>
+        <div className="col-4">
+          <h2>Projekt</h2>
+        </div>
+        <div className="col-4">
+          <h2>Nadzór</h2>
+        </div>
+      </div>
+      <div className="row text-center mt-2">
+        <div className="col-4 px-4">
+          <p className="lead">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim et
+            officia maiores ullam quaerat vel, magni minus illum commodi saepe
+            eveniet totam nihil ipsam eum qui fuga dolore sunt iure!
+          </p>
+        </div>
+        <div className="col-4 px-4">
+          <p className="lead">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            non delectus iste perspiciatis nemo explicabo, facilis voluptate
+            quaerat officiis velit minus fugiat fugit. Officia, officiis ut.
+            Eaque cupiditate aliquid nihil!
+          </p>
+        </div>
+        <div className="col-4 px-4">
+          <p className="lead">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam
+            fugit placeat, perspiciatis sunt veniam nam ducimus saepe porro.
+            Debitis quas quasi repellat illum laboriosam.
+          </p>
+        </div>
       </div>
     </div>
   </div>
