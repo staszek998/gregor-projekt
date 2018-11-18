e384eafaa566ff48f91b555312900692a29077eb
Stanislaw <gregor.stanislaw@hotmail.com>
2018-11-18 12:59:36 +0100

    Fix slider height on mobile

--- src/jsx/components/page-specific/main/ComplexEngineering/ComplexEngineering.jsx
+++ src/jsx/components/page-specific/main/ComplexEngineering/ComplexEngineering.jsx
@@ -8,36 +8,36 @@ const ComplexEngineering = () => (
       <h1 className="display-4 text-center">Kompleksowa inżynieria</h1>
 
       <div className="row justify-content-center align-items-center mt-5 pt-4">
-        <div className="col-4 text-center">
+        <div className="col-12 col-lg-4 text-center">
           <i className="fas fa-archway" />
         </div>
-        <div className="col-4 text-center">
+        <div className="col-12 col-lg-4 text-center">
           <i className="fas fa-pencil-ruler" />
         </div>
-        <div className="col-4 text-center">
+        <div className="col-12 col-lg-4 text-center">
           <i className="fas fa-binoculars" />
         </div>
       </div>
       <div className="row text-center mt-4">
-        <div className="col-4">
+        <div className="col-12 col-lg-4">
           <h2>Architektura</h2>
         </div>
-        <div className="col-4">
+        <div className="col-12 col-lg-4">
           <h2>Projekt</h2>
         </div>
-        <div className="col-4">
+        <div className="col-12 col-lg-4">
           <h2>Nadzór</h2>
         </div>
       </div>
       <div className="row text-center mt-2">
-        <div className="col-4 px-4">
+        <div className="col-12 col-lg-4 px-4">
           <p className="lead">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim et
             officia maiores ullam quaerat vel, magni minus illum commodi saepe
             eveniet totam nihil ipsam eum qui fuga dolore sunt iure!
           </p>
         </div>
-        <div className="col-4 px-4">
+        <div className="col-12 col-lg-4 px-4">
           <p className="lead">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
             non delectus iste perspiciatis nemo explicabo, facilis voluptate
@@ -45,7 +45,7 @@ const ComplexEngineering = () => (
             Eaque cupiditate aliquid nihil!
           </p>
         </div>
-        <div className="col-4 px-4">
+        <div className="col-12 col-lg-4 px-4">
           <p className="lead">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam
             fugit placeat, perspiciatis sunt veniam nam ducimus saepe porro.
