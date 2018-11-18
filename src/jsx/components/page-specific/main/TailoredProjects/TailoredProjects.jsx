b3924fc7868d6cf31b2a62b978b6057e86be0298
Stanislaw <gregor.stanislaw@hotmail.com>
2018-11-18 11:41:11 +0100

    Start designing the TailoredProjects

---
+++ src/jsx/components/page-specific/main/TailoredProjects/TailoredProjects.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TailoredProjects = () => (
+  <div className="row tailored-projects bg-white py-5">
+    <div className="col-12">
+      <h1 className="display-4 text-center">Projekty szyte na miarę</h1>
+
+      <div className="row justify-content-center mt-5">
+        <div className="col-3">col</div>
+        <div className="col-3">col</div>
+        <div className="col-3">col</div>
+      </div>
+    </div>
+  </div>
+);
+
+export default TailoredProjects;
--- src/jsx/pages/Main.jsx
+++ src/jsx/pages/Main.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 
 import HeroSlider from "../components/page-specific/main/HeroSlider/HeroSlider";
+import TailoredProjects from "../components/page-specific/main/TailoredProjects/TailoredProjects";
 
 const Main = () => (
   <main className="page--main">
     <div className="container">
       <div className="row">
-        <div className="col">
+        <div className="col px-0">
           <HeroSlider />
         </div>
       </div>
+      <TailoredProjects />
     </div>
   </main>
 );
