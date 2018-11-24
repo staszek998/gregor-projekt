943465a5bd2e089394b53cdc5969ee45d5e8217f
Stanislaw <gregor.stanislaw@hotmail.com>
2018-11-24 19:50:16 +0100

    Create the PortfolioBrief

---
+++ src/jsx/components/page-specific/main/PortfolioBrief/PortfolioBrief.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PortfolioBrief = () => (
+  <div className="PortfolioBrief row">
+    <div className="col">PortfolioBrief</div>
+  </div>
+);
+
+export default PortfolioBrief;
