199e17bf04b15e6926a6d1811591d70220d4be27
Stanislaw <gregor.stanislaw@hotmail.com>
2018-11-24 17:23:34 +0100

    Apply the proper column layout. Make the typography responsive

--- src/jsx/components/page-specific/main/ComplexEngineering/ComplexEngineering.jsx
+++ src/jsx/components/page-specific/main/ComplexEngineering/ComplexEngineering.jsx
@@ -1,57 +1,38 @@
 import React from "react";
 
 import "./_ComplexEngineering.scss";
+import Column from "./Column";
+
+const data = {
+  column1: {
+    icon: "fas fa-archway",
+    heading: "Architektura",
+    body:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim et officia maiores ullam quaerat vel, magni minus illum commodi saepe eveniet totam nihil ipsam eum qui fuga dolore sunt iure!"
+  },
+  column2: {
+    icon: "fas fa-pencil-ruler",
+    heading: "Projekt",
+    body:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus non delectus iste perspiciatis nemo explicabo, facilis voluptate quaerat officiis velit minus fugiat fugit. Officia, officiis ut. Eaque cupiditate aliquid nihil!"
+  },
+  column3: {
+    icon: "fas fa-binoculars",
+    heading: "Nadzór",
+    body:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam fugit placeat, perspiciatis sunt veniam nam ducimus saepe porro. Debitis quas quasi repellat illum laboriosam."
+  }
+};
 
 const ComplexEngineering = () => (
-  <div className="row complex-eingineering bg-white py-5">
+  <div className="ComplexEngineering row bg-white pt-5">
     <div className="col-12">
-      <h1 className="display-4 text-center">Kompleksowa inżynieria</h1>
+      <h1 className="display-4 text-center mb-5">Kompleksowa inżynieria</h1>
 
-      <div className="row justify-content-center align-items-center mt-5 pt-4">
-        <div className="col-12 col-lg-4 text-center">
-          <i className="fas fa-archway" />
-        </div>
-        <div className="col-12 col-lg-4 text-center">
-          <i className="fas fa-pencil-ruler" />
-        </div>
-        <div className="col-12 col-lg-4 text-center">
-          <i className="fas fa-binoculars" />
-        </div>
-      </div>
-      <div className="row text-center mt-4">
-        <div className="col-12 col-lg-4">
-          <h2>Architektura</h2>
-        </div>
-        <div className="col-12 col-lg-4">
-          <h2>Projekt</h2>
-        </div>
-        <div className="col-12 col-lg-4">
-          <h2>Nadzór</h2>
-        </div>
-      </div>
-      <div className="row text-center mt-2">
-        <div className="col-12 col-lg-4 px-4">
-          <p className="lead">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim et
-            officia maiores ullam quaerat vel, magni minus illum commodi saepe
-            eveniet totam nihil ipsam eum qui fuga dolore sunt iure!
-          </p>
-        </div>
-        <div className="col-12 col-lg-4 px-4">
-          <p className="lead">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
-            non delectus iste perspiciatis nemo explicabo, facilis voluptate
-            quaerat officiis velit minus fugiat fugit. Officia, officiis ut.
-            Eaque cupiditate aliquid nihil!
-          </p>
-        </div>
-        <div className="col-12 col-lg-4 px-4">
-          <p className="lead">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam
-            fugit placeat, perspiciatis sunt veniam nam ducimus saepe porro.
-            Debitis quas quasi repellat illum laboriosam.
-          </p>
-        </div>
+      <div className="row">
+        <Column {...data.column1} />
+        <Column {...data.column2} />
+        <Column {...data.column3} />
       </div>
     </div>
   </div>
