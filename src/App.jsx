2321c3e13f87fc6fb0db190bc35dcb994a3f35f6
Stanislaw <gregor.stanislaw@hotmail.com>
2018-11-25 15:44:23 +0100

    Create the Portfolio page

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import React, { Component } from "react";
 import "./App.scss";
 
 import Navbar from "./jsx/components/shared/Navbar/Navbar";
-import Main from "./jsx/pages/Main/Main";
 import Footer from "./jsx/components/shared/Footer/Footer";
+// import Main from "./jsx/pages/Main/Main";
+import Portfolio from "./jsx/pages/Portfolio/Portfolio";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
-        <Main />
+        <Portfolio />
         <Footer />
       </div>
     );
---
+++ src/jsx/pages/Portfolio/Portfolio.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import './_Portfolio.scss';
+
+const Portfolio = () => (
+  <main className="Portfolio ">
+    <div className="container">Portfolio</div>
+  </main>
+);
+
+export default Portfolio;
