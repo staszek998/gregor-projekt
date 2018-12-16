7e12f7302b4eea6f1f07e20fb78aecd825b9827c
Stanislaw <gregor.stanislaw@hotmail.com>
2018-12-16 19:33:19 +0100

    CReate the history component/page. Include it in the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Navbar from "./jsx/components/shared/Navbar/Navbar";
 import Footer from "./jsx/components/shared/Footer/Footer";
 import Main from "./jsx/pages/Main/Main";
 import Portfolio from "./jsx/pages/Portfolio/Portfolio";
+import History from "./jsx/pages/History/History";
 
 class App extends Component {
   render() {
@@ -18,6 +19,7 @@ class App extends Component {
           <Switch>
             <Route path="/" exact component={Main} />
             <Route path="/portfolio" exact component={Portfolio} />
+            <Route path="/historia" exact component={History} />
           </Switch>
 
           <Footer />
---
+++ src/jsx/pages/History/History.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const History = () => (
+  <main className="History container">
+    {/* Heading */}
+    <div className="row">
+      <div className="col-12 text-center">
+        <h1 className="display-1 mt-5 mb-4">Historia</h1>
+      </div>
+    </div>
+  </main>
+);
+
+export default History;
