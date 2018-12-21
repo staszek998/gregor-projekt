eaf402f3ae50c69c50d9e8b4447401a1a7455d3b
Stanislaw <gregor.stanislaw@hotmail.com>
2018-12-21 22:16:06 +0100

    Start coding the Modal  component

---
+++ src/jsx/components/page-specific/Contact/Modal.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Modal = () => (
+  <div
+    className="modal fade"
+    id="page--contact__modal"
+    aria-labelledby="page--contact__modal__label"
+    tabindex="-1"
+    role="dialog"
+    aria-hidden="true"
+  >
+    <div className="modal-dialog" role="document">
+      <div className="modal-content">
+        <div className="modal-header">
+          {/* Header */}
+          <h5 className="modal-title" id="page--contact__modal__label">
+            {header}
+          </h5>
+          <button
+            type="button"
+            className="close"
+            data-dismiss="modal"
+            aria-label="Close"
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+
+        {/* Body */}
+        <div className="modal-body">{body}</div>
+
+        {/* Footer */}
+        {footer ? <div className="modal-footer">{footer}</div> : null}
+      </div>
+    </div>
+  </div>
+);
+
+export default Modal;
---
+++ src/jsx/components/page-specific/Contact/ModalTrigger.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import ArrowLink from "../../shared/ArrowLink/ArrowLink";
+
+const ModalTrigger = ({ modalId, text }) => {};
+
+export default ModalTrigger;
