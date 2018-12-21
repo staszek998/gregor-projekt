5a4732140bc02829b799d36a5f4d5f20905c204c
Stanislaw <gregor.stanislaw@hotmail.com>
2018-12-21 22:05:25 +0100

    Create the basic layout for the page

--- src/jsx/pages/Contact/Contact.jsx
+++ src/jsx/pages/Contact/Contact.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-
 import "./_Contact.scss";
+import ArrowLink from "../../components/shared/ArrowLink/ArrowLink";
 
 const Contact = () => (
   <main className="Contact container">
@@ -8,6 +8,33 @@ const Contact = () => (
     <div className="row">
       <div className="col-12 text-center">
         <h1 className="display-1 mt-5 mb-4">Kontakt</h1>
+      </div>
+    </div>
+
+    {/* Main content */}
+    <div className="row bg-white text-center pb-md-5">
+      <div className="col-12 py-5">
+        <h2>Wybierz swoją ulubioną formę kontaktu</h2>
+      </div>
+
+      <div className="col-12 col-md-4">
+        <ArrowLink text="Wpadnę do biura" href="#" />
+      </div>
+      <div className="col-12 col-md-4">
+        <ArrowLink text="Porozmawiajmy przez telefon" href="#" />
+      </div>
+      <div className="col-12 col-md-4">
+        <ArrowLink text="Napiszę mail'a" href="#" />
+      </div>
+    </div>
+
+    {/* Quick links */}
+    <div className="quick-links row justify-content-around bg-dark text-center">
+      <div className="col-12 col-md-4 py-5">
+        <ArrowLink text="Wróć na stronę główną" href="#" white={true} />
+      </div>
+      <div className="col-12 col-md-4 py-5">
+        <ArrowLink text="Obejrzyj nasze projekty" href="#" white={true} />
       </div>
     </div>
   </main>
