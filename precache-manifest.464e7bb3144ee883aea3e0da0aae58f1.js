af22d34b049f8d0164f356dbb9ffbcfa6be13d4c
Stanislaw <gregor.stanislaw@hotmail.com>
2018-12-23 18:36:21 +0100

    Updates

---
+++ precache-manifest.464e7bb3144ee883aea3e0da0aae58f1.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = [
+  {
+    "revision": "723b02e678aeb292d8eeb2c6dfab425c",
+    "url": "/gregor-projekt/static/media/plan.723b02e6.png"
+  },
+  {
+    "revision": "6ca4cf191184f22fef28",
+    "url": "/gregor-projekt/static/css/main.d9c06fe9.chunk.css"
+  },
+  {
+    "revision": "dedcbe283dd9280c2d5551665d2bf12c",
+    "url": "/gregor-projekt/static/media/deco2.dedcbe28.svg"
+  },
+  {
+    "revision": "834cef080ca94d2dd003",
+    "url": "/gregor-projekt/static/js/1.834cef08.chunk.js"
+  },
+  {
+    "revision": "5a4ffd9a7eca0a548f64",
+    "url": "/gregor-projekt/static/js/runtime~main.5a4ffd9a.js"
+  },
+  {
+    "revision": "99363c304a2fb890f82fbfe07b9e0e15",
+    "url": "/gregor-projekt/static/media/1.99363c30.jpg"
+  },
+  {
+    "revision": "337b2134d1cdf632655d0cc5c3ee69ae",
+    "url": "/gregor-projekt/static/media/2.337b2134.jpg"
+  },
+  {
+    "revision": "b091b588daebca250e2c40be578a7b2c",
+    "url": "/gregor-projekt/static/media/3.b091b588.jpg"
+  },
+  {
+    "revision": "b9f0b7dfc664f2d8a197aa1910481e9f",
+    "url": "/gregor-projekt/static/media/4.b9f0b7df.jpg"
+  },
+  {
+    "revision": "ffc4dc428a2c6f3f55e910cdb7adc160",
+    "url": "/gregor-projekt/static/media/5.ffc4dc42.jpg"
+  },
+  {
+    "revision": "2dfd4ee23d2d1e6abfd4f1f9a6d1423f",
+    "url": "/gregor-projekt/static/media/img.2dfd4ee2.jpg"
+  },
+  {
+    "revision": "3bf5930bec249de464b81e36af4989e7",
+    "url": "/gregor-projekt/static/media/img.3bf5930b.jpg"
+  },
+  {
+    "revision": "f7b4ed48d66564869f1aea090504eb21",
+    "url": "/gregor-projekt/static/media/img.f7b4ed48.jpg"
+  },
+  {
+    "revision": "b385656ba5a13697b5cbef1694ebee85",
+    "url": "/gregor-projekt/static/media/house-1.b385656b.jpg"
+  },
+  {
+    "revision": "ec02c237c4eb10dc28889e9bfdb4bd46",
+    "url": "/gregor-projekt/static/media/house-2.ec02c237.jpg"
+  },
+  {
+    "revision": "db435e86052889ba0e08a80386cd9bac",
+    "url": "/gregor-projekt/static/media/house-3.db435e86.jpg"
+  },
+  {
+    "revision": "c989f6c8e4997dee726fef50b8eb5578",
+    "url": "/gregor-projekt/static/media/house-4.c989f6c8.jpg"
+  },
+  {
+    "revision": "231a57e54c030e5c2ac7f4b72d91d864",
+    "url": "/gregor-projekt/static/media/house-5.231a57e5.jpg"
+  },
+  {
+    "revision": "6fad3902f7e532c655cb19ffc1fa4b6d",
+    "url": "/gregor-projekt/static/media/house-6.6fad3902.jpg"
+  },
+  {
+    "revision": "6ca4cf191184f22fef28",
+    "url": "/gregor-projekt/static/js/main.6ca4cf19.chunk.js"
+  },
+  {
+    "revision": "e96dd742fc9b4ce389bf7393ba003a8b",
+    "url": "/gregor-projekt/static/media/interior.e96dd742.png"
+  },
+  {
+    "revision": "05746ca40e75e141964a09290ad4b987",
+    "url": "/gregor-projekt/static/media/4.05746ca4.jpg"
+  },
+  {
+    "revision": "b5cc7fa8be0c30b640646e8ee5fff73d",
+    "url": "/gregor-projekt/static/media/3.b5cc7fa8.jpg"
+  },
+  {
+    "revision": "4335dd3f5d466d2261699dc9814f4c84",
+    "url": "/gregor-projekt/static/media/5.4335dd3f.jpg"
+  },
+  {
+    "revision": "38b6d007f8432e15ed7bc1d1fbac3bdf",
+    "url": "/gregor-projekt/static/media/profile-photo-2.38b6d007.jpg"
+  },
+  {
+    "revision": "0545c4db399dff5afb54e5f476a2b5f0",
+    "url": "/gregor-projekt/static/media/Flaticon.0545c4db.eot"
+  },
+  {
+    "revision": "b6fadffb72fc58b63c27ea4a966243d5",
+    "url": "/gregor-projekt/static/media/Flaticon.b6fadffb.ttf"
+  },
+  {
+    "revision": "a5e3c4112c4d3c8f46acaa418b32a572",
+    "url": "/gregor-projekt/static/media/Flaticon.a5e3c411.svg"
+  },
+  {
+    "revision": "fc3c7f9bf0ddc2960d4caed4d4d714d9",
+    "url": "/gregor-projekt/static/media/Flaticon.fc3c7f9b.woff"
+  },
+  {
+    "revision": "d47c90564a36010d01fe64e3d6d357eb",
+    "url": "/gregor-projekt/static/media/Flaticon.d47c9056.ttf"
+  },
+  {
+    "revision": "89525b57f33ac1277c6ba9c1afa601d0",
+    "url": "/gregor-projekt/static/media/Flaticon.89525b57.woff"
+  },
+  {
+    "revision": "5935efed78bf93ca9bfaffa0054372da",
+    "url": "/gregor-projekt/static/media/Flaticon.5935efed.eot"
+  },
+  {
+    "revision": "495b5d6e74161279a171502a9d3812db",
+    "url": "/gregor-projekt/static/media/Flaticon.495b5d6e.svg"
+  },
+  {
+    "revision": "f46f486edefeba66ac4b538dd593d656",
+    "url": "/gregor-projekt/static/media/Quicksand-Regular.f46f486e.ttf"
+  },
+  {
+    "revision": "166b7283b0af2b96287c91e6c6a0dc73",
+    "url": "/gregor-projekt/static/media/Quicksand-Bold.166b7283.ttf"
+  },
+  {
+    "revision": "043a37371ee14e929d993990a3ad18db",
+    "url": "/gregor-projekt/static/media/Quicksand-Light.043a3737.ttf"
+  },
+  {
+    "revision": "6080ccc3a807724b6ff10b7b609137da",
+    "url": "/gregor-projekt/static/media/Quicksand-Medium.6080ccc3.ttf"
+  },
+  {
+    "revision": "834cef080ca94d2dd003",
+    "url": "/gregor-projekt/static/css/1.b6746b52.chunk.css"
+  },
+  {
+    "revision": "f80721baa652a172c39968c35c17d6e4",
+    "url": "/gregor-projekt/index.html"
+  }
+];
